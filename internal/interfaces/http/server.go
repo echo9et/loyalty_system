44b502ee95a60273346a1ad74f1bc397587383b1
echo9et <echo9et@yandex.ru>
2025-03-18 15:02:43 +0300

    1

--- internal/interfaces/http/server.go
+++ internal/interfaces/http/server.go
@@ -35,7 +35,7 @@ func New() (*Server, error) {
 	server.Engine.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", c.GetHeader("Origin"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
 
 		if c.Request.Method != "POST" && c.Request.Method != "GET" {
